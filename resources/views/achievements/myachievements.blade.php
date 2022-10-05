4b61f5076e2d95a09af220d632c987be77294708
Vladislav Prosekov <vprosekoff@gmail.com>
2022-10-05 21:32:24 +0600

    myachievements верстка

--- resources/views/achievements/myachievements.blade.php
+++ resources/views/achievements/myachievements.blade.php
@@ -2,9 +2,10 @@
 
 @section('styles')
     <style>
-        html{
+        html {
             /* height: 100vh; */
         }
+
         a {
             text-decoration: none;
             color: white;
@@ -64,7 +65,7 @@
             letter-spacing: -0.04em;
 
             color: #FFFFFF;
-			margin-bottom:2px;
+            margin-bottom: 2px;
         }
 
         .studentname {
@@ -99,22 +100,22 @@
         }
 
         .topinstudents {
-			margin-top:20px;
+            margin-top: 20px;
             margin-left: auto;
             margin-right: auto;
             width: fit-content;
             font-style: normal;
-			font-weight: 700;
-			font-size: 54px;
-			line-height: 80%;
-			/* or 29px */
+            font-weight: 700;
+            font-size: 54px;
+            line-height: 80%;
+            /* or 29px */
 
-			text-align: center;
-			letter-spacing: -0.04em;
+            text-align: center;
+            letter-spacing: -0.04em;
 
-			color: #372370;
+            color: #372370;
             margin-bottom: 4px;
-			text-align:center;
+            text-align: center;
             font-family: 'Montserrat-Bold';
         }
 
@@ -123,16 +124,16 @@
             margin-right: auto;
             width: fit-content;
             font-style: normal;
-			font-weight: 400;
-			font-size: 8.5px;
-			line-height: 80%;
-			/* or 7px */
+            font-weight: 400;
+            font-size: 8.5px;
+            line-height: 80%;
+            /* or 7px */
 
-			text-align: center;
-			letter-spacing: -0.04em;
+            text-align: center;
+            letter-spacing: -0.04em;
 
-			color: #372370;
-			text-align:center;
+            color: #372370;
+            text-align: center;
 
         }
 
@@ -145,58 +146,143 @@
         .maincontent {
             height: 100%;
             width: auto;
-			display:flex;
-			flex-direction:column;
-			background: #FFFFFF;
-			box-shadow: 0px 1px 25px #331C74;
-			border-radius: 50px 50px 0px 0px;
+            display: flex;
+            flex-direction: column;
+            background: #FFFFFF;
+            box-shadow: 0px 1px 25px #331C74;
+            border-radius: 50px 50px 0px 0px;
         }
-		.achievements-buttons{
-			margin-top:15px;
-			display:flex;
-			flex-direction:row;
-		}
-		.achievement-control-btn{
-			width:80%;
-			background: #FFFFFF;
-			box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
-			border-radius: 15px;
-			font-style: normal;
-			font-weight: 400;
-			font-size: 16px;
-			line-height: 80%;
-			/* or 10px */
 
-			letter-spacing: -0.04em;
+        .achievements-buttons {
+            margin-top: 15px;
+            display: flex;
+            flex-direction: row;
+        }
 
-			color: #000000;
-			padding: 10px;
-			margin-left:auto;
-			margin-right: auto;
-			text-align:center
-		}
+        .achievement-control-btn {
+            width: 80%;
+            background: #FFFFFF;
+            box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+            border-radius: 15px;
+            font-style: normal;
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 80%;
+            /* or 10px */
+
+            letter-spacing: -0.04em;
+
+            color: #000000;
+            padding: 10px;
+            margin-left: auto;
+            margin-right: auto;
+            text-align: center
+        }
+
+        .achievements-list {
+            margin-top: 18px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+        }
+
+        .achievement {
+            background: #FFFFFF;
+            box-shadow: 0px -5px 34px -2px rgba(0, 0, 0, 0.15);
+            border-radius: 5px;
+            padding: 10px;
+            width: 95%;
+            display: flex;
+            flex-direction: row;
+        }
+
+        .left {
+            width: fit-content;
+        }
+
+        .achievement-name {
+            font-style: normal;
+            font-weight: 600;
+            font-size: 12px;
+            line-height: 80%;
+            /* or 10px */
+
+            letter-spacing: -0.04em;
+
+            color: #484848;
+        }
+
+        .achievement-description {
+            font-style: normal;
+            font-weight: 400;
+            font-size: 12px;
+            line-height: 80%;
+            /* or 10px */
+
+            letter-spacing: -0.025em;
+
+            color: #878787;
+            margin-top: 10px;
+        }
+
+        .scores {
+            width: auto;
+            margin-left: auto;
+            margin-right: 0;
+            display: flex;
+            flex-direction: column;
+        }
+
+        .scores-value{
+            margin-bottom: 5px;
+        }
+
+        .scores>* {
+            font-style: normal;
+            font-weight: 600;
+            font-size: 14px;
+            line-height: 80%;
+            /* or 11px */
+
+            text-align: center;
+            letter-spacing: -0.04em;
+
+            color: #484848;
+        }
     </style>
 @endsection
 
 @section('content')
-  <header>
-      <div class="front">
-          <div class="profilephotodiv"><img src="{{$student[0]['photo']}}"
-                  class="profilephoto"></div>
-          <div class="studentid">#{{$student[0]['id']}}</div>
-          <div class="studentname">{{$student[0]['name']}}</div>
-          <div class="shanyraqclass">{{$student[0]['shanyraqName'].' Shanyraqy | '.$student[0]['grade']}}</div>
-      </div>
-  </header>
-  <div class="maincontent">
-          <div class="topinstudents">#{{$student[0]['rank']}}</div>
-          <div class="understtoplable">IN {{env('SCHOOL_NAME')}} students rating</div>
-      <div class="achievements-buttons">
-		  <div class="achievement-control-btn" id="suggest-achievement">
-			  Предложить достижение
-		  </div>
-	  </div>
-	  
-  </div>
+    <header>
+        <div class="front">
+            <div class="profilephotodiv"><img src="{{ $student[0]['photo'] }}" class="profilephoto"></div>
+            <div class="studentid">#{{ $student[0]['id'] }}</div>
+            <div class="studentname">{{ $student[0]['name'] }}</div>
+            <div class="shanyraqclass">{{ $student[0]['shanyraqName'] . ' Shanyraqy | ' . $student[0]['grade'] }}</div>
+        </div>
+    </header>
+    <div class="maincontent">
+        <div class="topinstudents">#{{ $student[0]['rank'] }}</div>
+        <div class="understtoplable">IN {{ env('SCHOOL_NAME') }} students rating</div>
+        <div class="achievements-buttons">
+            <div class="achievement-control-btn" id="suggest-achievement">
+                Предложить достижение
+            </div>
+        </div>
+        <div class="achievements-list">
+            <div class="achievement">
+                <div class="left">
+                    <div class="achievement-name"><strong>Школьный победитель</strong></div>
+                    
+                    <div class="achievement-description">Победитель школьного конкурса</div>
+                </div>
+                <div class="scores">
+                    <div class="scores-value">228</div>
+                    <div class="scoresname">баллов</div>
+                </div>
+            </div>
+        </div>
 
+    </div>
 @endsection
